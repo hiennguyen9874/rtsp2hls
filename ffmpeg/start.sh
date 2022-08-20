86d9c644b0cf46dbf5c13bd234acbc9b711a7bd7
Xuan Hien <hiennguyen9874@gmail.com>
2022-08-20 12:18:26 +0700

    Update ffmpeg comain

--- ffmpeg/start.sh
+++ ffmpeg/start.sh
@@ -2,18 +2,29 @@
 
 if [ "$SOURCE_URL" != "" ]; then\
     echo "Start!"
+    # ffmpeg -rtsp_transport tcp \
+    #     -i $SOURCE_URL \
+    #     -fflags flush_packets \
+    #     -max_delay 2 \
+    #     -c:v libx265 -vtag hvc1 \
+    #     -flags -global_header \
+    #     -hls_segment_type fmp4 \
+    #     -hls_time 2 \
+    #     -hls_list_size 3 \
+    #     -hls_flags delete_segments+append_list+split_by_time \
+    #     -hls_playlist_type event \
+    #     -vcodec copy \
+    #     -y /tmp/stream/index.m3u8
     ffmpeg -rtsp_transport tcp \
-        -i $SOURCE_URL \
-        -fflags flush_packets \
-        -max_delay 2 \
-        -c:v libx265 -vtag hvc1 \
         -flags -global_header \
+        -i $SOURCE_URL \
+        -an -c:v copy \
+        -f hls \
+        -lhls 1 \
+        -hls_time 1 \
+        -hls_list_size 4 \
+        -hls_wrap 5 \
         -hls_segment_type fmp4 \
-        -hls_time 2 \
-        -hls_list_size 3 \
-        -hls_flags delete_segments+append_list+split_by_time \
-        -hls_playlist_type event \
-        -vcodec copy \
         -y /tmp/stream/index.m3u8
 else
     echo "Won't restream a source feed to the server because SOURCE_URL was not defined"
